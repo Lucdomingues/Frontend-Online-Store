44e71955628a397a00a544a6e8a59057462241e5
Lucdomingues <lojalucc@outlook.com>
2023-03-21 00:00:06 -0300

    deploy

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home';
 import Carrinho from './components/Carrinho';
@@ -8,13 +8,13 @@ import Card from './components/Card';
 class App extends React.Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <Switch>
           <Route exact path="/" component={ Home } />
           <Route exact path="/carrinho" component={ Carrinho } />
           <Route exact path="/card/:id" render={ (props) => <Card { ...props } /> } />
         </Switch>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
